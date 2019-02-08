fd59adae51f47e5d8d78c1e7b82206c8686456b5
victorely <victor.georges.ely@gmail.com>
2019-02-08 18:40:51 +0100

    Correction lancement du script pour windows

--- script/launch.bat
+++ script/launch.bat
@@ -1,3 +1,2 @@
-set /p Input=Entrer l'id du dernier évènement du Cercle :
-set path=%cd%\bin
-java -jar cercle-1.0-SNAPSHOT-jar-with-dependencies.jar %Input%
+set /p Input=Entrer l'id du dernier evènement du cercle :
+"%cd%\jre\bin\java.exe" -jar cercle-1.0-SNAPSHOT-jar-with-dependencies.jar %Input%
