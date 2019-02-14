7b82d740c95fa7e411a6e4965ac4daa69c0ed5ae
victorely <victor.georges.ely@gmail.com>
2019-02-14 23:32:40 +0100

    Small fix

--- src/main/java/com/victorely/cercle/Cercle.java
+++ src/main/java/com/victorely/cercle/Cercle.java
@@ -14,17 +14,18 @@ public class Cercle {
     private static final Logger logger = LogManager.getLogger(Cercle.class.getName());
 
     public static void main(String[] args) {
-        int lastEventId; //127973 ou 127960
+        int lastEventId; //127973 ou 127960 128406
         try {
             lastEventId = Integer.parseInt(args[0]);
         } catch (NumberFormatException e) {
             lastEventId = 127973; // Initialisation avec le dernier evenement fait à Paris la première semaine de février
         }
         int code = 0;
+        int j = 0;
         // Boucle pour tester tous les comptes du Cercle sur Paylogic
         for (String string : getSaleId()) {
             // Boucle pour tester les évènements du dernier + 1 jusqu'à 600 en plus
-            for (int i = lastEventId; i <= lastEventId + 600; i++) {
+            for (int i = lastEventId + 1; i <= lastEventId + 601; i++) {
                 String cercleUrl = "https://frontoffice.paylogic.nl/?event_id=" + i + "&point_of_sale_id=" + string;
                 URL url = null;
                 HttpURLConnection connection;
@@ -35,10 +36,11 @@ public class Cercle {
                     connection.setRequestMethod("GET");
                     connection.connect();
                     code = connection.getResponseCode();
-                    connection.disconnect();
+                    connection.disconnect(); // On ferme la connexion pour limiter les ressources
                 } catch (IOException e) {
                     logger.error(e.getMessage(), e);
                 }
+                logger.info("Temps restant avant la fin du programme : " + (600 * getSaleId().size() - j));
                 // Si l'on va ici, c'est gagné !
                 if (code == 200) {
                     // Ouvrir le navigateur par défaut avec la page en question !
@@ -52,6 +54,12 @@ public class Cercle {
                     }
                     logger.info(url);
                 }
+                else if (code == 429) {
+                    logger.error("Trop de requêtes");
+                }
+                else {
+                    logger.error("Code retour erreur : " + code);
+                }
                 // On applique ici une pause pour ne pas avoir le code retour HTTP 429
                 try {
                     Thread.sleep(500);
@@ -59,10 +67,12 @@ public class Cercle {
                     logger.error(e.getMessage(), e);
                     Thread.currentThread().interrupt();
                 }
+                j++;
             }
         }
     }
 
+    // List des ID du Cercle déjà existant
     private static List<String> getSaleId() {
         List<String> list = new ArrayList<>();
         list.add("17782");
