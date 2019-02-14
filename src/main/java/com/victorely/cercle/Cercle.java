7b6567bcc6c3a881d08818d85434f530a70bea65
victorely <victor.georges.ely@gmail.com>
2019-02-14 23:56:05 +0100

    Small fix

--- src/main/java/com/victorely/cercle/Cercle.java
+++ src/main/java/com/victorely/cercle/Cercle.java
@@ -5,13 +5,18 @@ import org.apache.logging.log4j.Logger;
 
 import java.awt.*;
 import java.io.IOException;
-import java.net.*;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Cercle {
 
     private static final Logger logger = LogManager.getLogger(Cercle.class.getName());
+
+    private static final int NOMBRE_TEST = 601;
 
     public static void main(String[] args) {
         int lastEventId; //127973 ou 127960 128406
@@ -23,9 +28,10 @@ public class Cercle {
         int code = 0;
         int j = 0;
         // Boucle pour tester tous les comptes du Cercle sur Paylogic
-        for (String string : getSaleId()) {
+        List<String> saleId = getSaleId();
+        for (String string : saleId) {
             // Boucle pour tester les évènements du dernier + 1 jusqu'à 600 en plus
-            for (int i = lastEventId + 1; i <= lastEventId + 601; i++) {
+            for (int i = lastEventId + 1; i <= lastEventId + NOMBRE_TEST; i++) {
                 String cercleUrl = "https://frontoffice.paylogic.nl/?event_id=" + i + "&point_of_sale_id=" + string;
                 URL url = null;
                 HttpURLConnection connection;
@@ -40,7 +46,6 @@ public class Cercle {
                 } catch (IOException e) {
                     logger.error(e.getMessage(), e);
                 }
-                logger.info("Temps restant avant la fin du programme : " + (600 * getSaleId().size() - j));
                 // Si l'on va ici, c'est gagné !
                 if (code == 200) {
                     // Ouvrir le navigateur par défaut avec la page en question !
@@ -53,13 +58,10 @@ public class Cercle {
                         }
                     }
                     logger.info(url);
-                }
-                else if (code == 429) {
+                } else if (code == 429) {
                     logger.error("Trop de requêtes");
                 }
-                else {
-                    logger.error("Code retour erreur : " + code);
-                }
+                logger.info("Temps restant avant la fin du programme : " + (NOMBRE_TEST * saleId.size() - j) + " seconde(s)");
                 // On applique ici une pause pour ne pas avoir le code retour HTTP 429
                 try {
                     Thread.sleep(500);
